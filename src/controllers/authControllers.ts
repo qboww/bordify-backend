a23171960d5e6e4d38eda7a072a3945a18023827
Yevhenii Sarancha <yevhenii.sarancha@sigma.software>
2025-05-14 22:58:19 +0300

    google auth fix

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -358,9 +358,10 @@ const googleRedirect: Controller = async (req, res) => {
       refreshToken,
     });
 
-    return res.redirect(
-      `${env('FRONTEND_URL')}?sid=${newSession._id}&accessToken=${newSession.accessToken}&refreshToken=${newSession.refreshToken}`
-    );
+    const redirectUrl = new URL(env('FRONTEND_URL'));
+    redirectUrl.hash = `/auth/google-redirect?sid=${newSession._id}&accessToken=${accessToken}&refreshToken=${refreshToken}`;
+
+    return res.redirect(redirectUrl.toString());
   }
 
   const { _id } = user;
